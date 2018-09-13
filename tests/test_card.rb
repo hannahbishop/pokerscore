8cb3d0324a5b36f249e7b04b9874799b7d175c67
Hannah Bishop <hbishop@juul-hbishop-imacpro.local>
2018-09-13 15:13:15 -0700

    add missing test assertion

--- tests/test_card.rb
+++ tests/test_card.rb
@@ -5,5 +5,6 @@ require File.expand_path('../../lib/card.rb', __FILE__)
 class TestCard < Minitest::Test
     def test_can_create_card_object
         card = Card.new
-    end
+        assert_equal card.class, Card
+    end 
 end
