8c469e7d61908d08fe0e5eb6e9089873a9d9f5a1
Hannah Bishop <hbishop@juul-hbishop-imacpro.local>
2018-09-14 13:47:49 -0700

    add card equals definition

--- lib/card.rb
+++ lib/card.rb
@@ -2,6 +2,7 @@ require File.expand_path('../../const/card_constants.rb', __FILE__)
 
 class Card
   attr_reader :value, :suit
+
   def initialize(value, suit)
     if $valid_suits.include? suit and $valid_values.include? value
       @suit = suit
@@ -9,5 +10,9 @@ class Card
     else
       raise ArgumentError, "Invalid card specifications. Given suit #{suit} and value #{value}"
     end
+  end
+
+  def ==(other_card)
+    return (other_card.class == Card and suit == other_card.suit and value == other_card.value)
   end
 end
