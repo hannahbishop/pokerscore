569a11f11cce7edc1f7cca75c5edaa0b34b7e668
Hannah Bishop <hbishop@juul-hbishop-imacpro.local>
2018-09-14 10:06:35 -0700

    add card suit and value

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,13 @@
+require File.expand_path('../../const/card_constants.rb', __FILE__)
+
 class Card
+  attr_reader :value, :suit
+  def initialize(value, suit)
+    if $valid_suits.include? suit and $valid_values.include? value
+      @suit = suit
+      @value = value
+    else
+      raise Exception, "Invalid card specifications. Given suit #{suit} and value #{value}"
+    end
+  end
 end
--- tests/test_card.rb
+++ tests/test_card.rb
@@ -1,10 +1,33 @@
 require 'minitest/autorun'
 require 'minitest/color'
 require File.expand_path('../../lib/card.rb', __FILE__)
+require File.expand_path('../../const/card_constants.rb', __FILE__)
 
 class TestCard < Minitest::Test
-    def test_can_create_card_object
-        card = Card.new
-        assert_equal card.class, Card
-    end 
+  def test_can_create_card_object
+    card = Card.new(3, :heart)
+    assert_equal card.class, Card
+  end
+  def test_card_object_has_suit_and_value
+    card = Card.new(3, :heart)
+    assert_equal card.value, 3
+    assert_equal card.suit, :heart
+  end
+  def test_card_object_rejects_invalid_suit
+    assert_raises Exception do
+      card = Card.new(3, :hearts)
+    end
+  end 
+  def test_card_object_rejects_value_too_small
+    too_small = $valid_values.min - 1
+    assert_raises Exception do
+      card = Card.new(too_small, :heart)
+    end
+  end
+  def test_card_object_rejects_value_too_large
+    too_large = $valid_values.max + 1
+    assert_raises Exception do
+      card = Card.new(too_large, :heart)
+    end
+  end
 end
