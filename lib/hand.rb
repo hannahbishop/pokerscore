439ff9a6ad032877d348ec902caff695a65f069b
Hannah Bishop <hbishop@juul-hbishop-imacpro.office.stembolt.io>
2018-09-17 16:27:49 -0700

    add straight finder

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,13 +1,29 @@
 class Hand
-  attr_reader :cards
+  attr_reader :cards, :straight
   
   def initialize(cards)
     raise ArgumentError if invalid_hand?(cards)
     @cards = cards
+    @straight = find_straight(cards)
   end
 
   def invalid_hand?(cards)
     unique_cards = cards.uniq {|c| [c.suit, c.value]}
     unique_cards.length != 5
+  end
+
+  private
+
+  def find_straight(cards)
+    sorted = cards.sort_by { |c| c.value }
+    result = { 
+      :range => sorted.first.value..sorted.last.value,
+      :suit => sorted.first.suit
+    }
+    sorted.each_cons(2).each do |x,y|
+      result[:range] = nil if not x.value == y.value - 1
+      result[:suit] = :mixed if not x.suit == y.suit
+    end
+    result
   end
 end 
