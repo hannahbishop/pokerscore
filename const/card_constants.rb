d4ebeb4b4496cac73ca2c4ee8b589ddb5bc2cbe3
Hannah Bishop <hbishop@juul-hbishop-imacpro.local>
2018-09-20 11:20:04 -0700

    add to_s methods

--- const/card_constants.rb
+++ const/card_constants.rb
@@ -1,2 +1,8 @@
 $valid_suits = [:heart, :diamond, :spade, :club].freeze
-$valid_values = (2..14).freeze
+$valid_values =* (2..14).freeze
+$face_cards = {
+  $valid_values[-1] => "A",
+  $valid_values[-2] => "K",
+  $valid_values[-3] => "Q",
+  $valid_values[-4] => "J"
+}
