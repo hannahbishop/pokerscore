87420af8e97f585275f7ce6aa6af3de03ed4034d
Hannah Bishop <hannahbananabishop@gmail.com>
2018-10-10 11:45:04 -0700

    Clean up test files
    Several test files had incorrect method or class names left over from
    when they tested previous functionality. They have been changed to
    reflect the current design structure.

--- tests/texas_hold_em/test_evaluate.rb
+++ tests/texas_hold_em/test_evaluate.rb
@@ -1,31 +1,31 @@
 require 'minitest/autorun'
 require 'minitest/color'
-require 'tests/hand_factories.rb'
-require 'lib/pokerscore/texas_hold_em/evaluate.rb'
+require 'tests/hand_factories'
+require 'lib/pokerscore/texas_hold_em/evaluate'
 
-class TestHand < Minitest::Test
-  def test_hand_evaluator_accurately_compares_different_hand_types
+class TestEvaluate < Minitest::Test
+  def test_evaluate_finds_winner_from_different_hand_types
     flush = HandFactories.flush_hand(high_card_value = 10)
     straight = HandFactories.straight_hand(high_card_value = 7)
     evaluate = TexasHoldEm::Evaluate.new(flush, straight)
     assert_equal evaluate.call, flush
   end
 
-  def test_hand_evaluator_accurately_finds_tie_and_returns_nil
+  def test_evaluate_finds_tie_and_returns_nil
     trips1 = HandFactories.hand([2,2,2,5,6])
     trips2 = HandFactories.hand([2,2,2,5,6])
     evaluate = TexasHoldEm::Evaluate.new(trips1, trips2)
     assert_nil evaluate.call
   end
 
-  def test_hand_evaluator_accurately_finds_winner_wheel_straight_vs_straight
+  def test_evaluate_finds_winner_wheel_straight_vs_straight
     wheel_straight = HandFactories.hand([2,3,4,5,14])
     straight = HandFactories.straight_hand(high_card_value = 6)
     evaluate = TexasHoldEm::Evaluate.new(wheel_straight, straight)
     assert_equal evaluate.call, straight
   end
 
-  def test_hand_evaluator_accurately_finds_winner_sets_vs_sets
+  def test_evaluate_finds_winner_sets_vs_sets
     two_pair1 = HandFactories.two_pair_hand(pair_value1 = 5, pair_value2 = 3)
     two_pair2 = HandFactories.two_pair_hand(pair_value1 = 5, pair_value2 = 2)
     evaluate = TexasHoldEm::Evaluate.new(two_pair1, two_pair2)
--- tests/texas_hold_em/test_identify.rb
+++ tests/texas_hold_em/test_identify.rb
@@ -3,63 +3,63 @@ require 'minitest/color'
 require 'tests/hand_factories.rb'
 require 'lib/pokerscore/texas_hold_em/identify.rb'
 
-class TestHand < Minitest::Test
+class TestIdentify < Minitest::Test
 
-  def test_hand_evaluator_identifies_pair
+  def test_identify_finds_pair
     pair_hand = HandFactories.pair_hand(pair_value = 10)
     identify = TexasHoldEm::Identify.new(pair_hand)
     assert_equal identify.call, :pair
   end
 
-  def test_hand_evaluator_identifies_two_pair
+  def test_identify_finds_two_pair
     two_pair_hand = HandFactories.two_pair_hand(pair_value1 = 3, pair_value2 = 5)
     identify = TexasHoldEm::Identify.new(two_pair_hand)
     assert_equal identify.call, :two_pair
   end
 
-  def test_hand_evaluator_identifies_trips
+  def test_identify_finds_trips
     trips_hand = HandFactories.triplet_hand(triplet_value = 6)
     identify = TexasHoldEm::Identify.new(trips_hand)
     assert_equal identify.call, :three_of_a_kind
   end
 
-  def test_hand_evaluator_identifies_full_house
+  def test_identify_finds_full_house
     full_house_hand = HandFactories.full_house_hand(triplet = 5, pair = 2)
     identify = TexasHoldEm::Identify.new(full_house_hand)
     assert_equal identify.call, :full_house
   end
 
-  def test_hand_evaluator_identifies_quad
+  def test_identify_finds_quad
     quad_hand = HandFactories.four_of_a_kind_hand(four_of_a_kind_value = 6)
     identify = TexasHoldEm::Identify.new(quad_hand)
     assert_equal identify.call, :four_of_a_kind
   end
 
-  def test_hand_evaluator_identifies_royal_flush
+  def test_identify_finds_royal_flush
     royal_flush_hand = HandFactories.royal_flush_hand
     identify = TexasHoldEm::Identify.new(royal_flush_hand)
     assert_equal identify.call, :royal_flush
   end
 
-  def test_hand_evaluator_identifies_straight_flush
+  def test_identify_finds_straight_flush
     straight_flush_hand = HandFactories.straight_flush_hand(high_card_value = 10)
     identify = TexasHoldEm::Identify.new(straight_flush_hand)
     assert_equal identify.call, :straight_flush
   end
 
-  def test_hand_evaluator_identifies_straight
+  def test_identify_finds_straight
     straight_hand = HandFactories.straight_hand(high_card_value = 6)
     identify = TexasHoldEm::Identify.new(straight_hand)
     assert_equal identify.call, :straight
   end
 
-  def test_hand_evaluator_identifies_straight_from_wheel_straight
+  def test_identify_finds_straight_from_wheel_straight
     wheel_straight_hand = HandFactories.hand([2,3,4,5,14])
     identify = TexasHoldEm::Identify.new(wheel_straight_hand)
     assert_equal identify.call, :straight
   end
 
-  def test_hand_evaluator_identifies_flush
+  def test_identify_finds_flush
     flush_hand = HandFactories.flush_hand(high_card = 7)
     identify = TexasHoldEm::Identify.new(flush_hand)
     assert_equal identify.call, :flush
